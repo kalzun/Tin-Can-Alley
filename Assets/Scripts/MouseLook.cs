cc26108dfbb8e4c16763afc65f1153810afa97e0
Kalzun <erikand@ErikAnds-MacBook-Air.local>
2016-04-08 17:07:37 +0200

    Revert "Revert "First commit""
    This reverts commit 260833799a011bd33d1f288446473635f09998c5.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public Camera cam;
+	public GameObject prefabBall;
+	public float force;
+
+	Ray mouseRay;
+	RaycastHit hit;
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseRay = cam.ScreenPointToRay (Input.mousePosition);
+
+		if (Input.GetMouseButtonDown (0)){
+			if (Physics.Raycast(mouseRay)){
+				if (BallManager.thrownBalls < BallManager.maxBalls){
+					// Debug.DrawRay (mouseRay.origin, mouseRay.direction, Color.red, 4f); 
+					ThrowBall (BallManager.thrownBalls);
+				}
+			}	
+				
+		}
+	}
+
+	void ThrowBall(int balls){
+		GameObject ballInstance = Instantiate (prefabBall, mouseRay.origin, prefabBall.transform.rotation) as GameObject;
+		ballInstance.GetComponent<Rigidbody> ().AddForce (mouseRay.direction * force, ForceMode.Impulse);
+		DelegatesAndEvents.BallThrown(balls);
+	}
+}
